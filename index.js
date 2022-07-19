ecc202879e10dd6b11438ebcb16de84a49f5aa58
George Baroi <baroi.george@gmail.com>
2022-07-19 09:22:39 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1= 2
+let num2= 31
+let multiply = num1 * num2
+let random = Math.ceil(Math.random()+1)
+let num3 = 4
+let num4 = 25
+let mod = num3%num4
+const max = Math.max(1, 20)
 
-// Write your code here
+
+
+
